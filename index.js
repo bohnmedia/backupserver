6bcae0d0f34f18368141d5fbebb45d9a4d0996ac
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-07-07 21:19:51 +0200

    Anzahl von Backups nun einstellbar

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const Backup = require('./lib/Backup');
 const Config = require('./lib/Config');
 
 const config = new Config('./servers.txt');
+const options = {
+    backupsPerServer: {
+        hour: 3,
+        day: 7,
+        week: 4,
+        month: 3
+    }
+}
 
 const backupAll = async function(servers) {
 
     for (let i=0; i<servers.length; i++) {
+        const backup = new Backup(servers[i], options);
         console.log('Start backup: "' + servers[i].url + '"');
-        const backup = new Backup(servers[i]);
         await backup.start();
     }
 
