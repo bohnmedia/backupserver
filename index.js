5d44540b5cd452b2a52e0e08e8d7c61ed03dce01
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-07-07 16:54:23 +0200

    Diverse Anpassungen

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const Backup = require('./lib/Backup');
 const Config = require('./lib/Config');
-const config = new Config("/home/contaobackup/backupserver/urls.txt");
-var urls;
 
-config.getUrls().then(urls => {
-    console.log(urls);
-});
+const config = new Config('./servers.txt');
+
+const backupAll = async function(servers) {
+
+    for (let i=0; i<servers.length; i++) {
+        console.log('Start backup: "' + servers[i].url + '"');
+        const backup = new Backup(servers[i]);
+        await backup.start();
+    }
+
+}
+
+config.getServers(backupAll);
