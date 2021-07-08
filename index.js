e458cc9acd4ad966709dbd594397231ada1c0813
Sven Nehls <10372051+snehls@users.noreply.github.com>
2021-07-08 14:17:04 +0200

    Farbige Konsolenausgaben implementiert

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const Backup = require('./lib/Backup');
 const Config = require('./lib/Config');
+const Log = require('./lib/Log');
 
 const config = new Config('./servers.txt');
 const options = {
-    backupsPerServer: {
-        hour: 3,
-        day: 7,
-        week: 4,
-        month: 3,
-        year: 1
-    }
-}
+  backupsPerServer: {
+    hour: 3,
+    day: 7,
+    week: 4,
+    month: 3,
+    year: 1,
+  },
+};
 
-const backupAll = async function(servers) {
-
-    for (let i=0; i<servers.length; i++) {
-        const backup = new Backup(servers[i], options);
-        console.log('Start backup: "' + servers[i].url + '"');
-        await backup.start();
-    }
-
-}
+const backupAll = async function (servers) {
+  for (let i = 0; i < servers.length; i++) {
+    const backup = new Backup(servers[i], options);
+    Log.info('Start backup: "' + servers[i].url + '"');
+    await backup.start();
+  }
+};
 
 config.getServers(backupAll);
